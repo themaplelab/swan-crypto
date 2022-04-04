5c89a14b4103a2ba128939d6c4ea48b22f064d6c
Daniil Tiganov <tiganov@ualberta.ca>
2022-04-04 03:55:34 -0600

    Added tests

---
+++ CryptoSwiftTests/CryptoSwiftTests/main.swift
@@ -0,0 +1,362 @@
+import Foundation
+import CryptoSwift
+
+// *** UTILITIES ***
+
+func getRandomArray() -> Array<UInt8> {
+  AES.randomIV(10)
+}
+
+func getUnknownArray() -> Array<UInt8> {
+  [0x2a, 0x3a, 0x80, 0x05, 0xaf, 0x46, 0x58, 0x2d, 0x66, 0x52, 0x10, 0xae, 0x86, 0xd3, 0x8e, 0x8f]
+}
+
+func unknownCondition() -> Bool {
+  Bool.random()
+}
+
+func getConstantString() -> String {
+  "constant string"
+}
+
+func getConstantArray() -> Array<UInt8> {
+  Array<UInt8>(hex: "constant hex string")
+}
+
+func getLowIterationCount() -> Int { return 999 }
+
+func getGoodIterationCount() -> Int { return 1000 }
+
+// *** RULE 1: USING ECB BLOCK MODE FOR ENCRYPTION ***
+
+func test_r1_simple_violation() throws {
+  let key = getRandomArray()
+  let padding = Padding.noPadding
+  let blockMode = ECB()
+  
+  // Violation SHOULD be detected only for blockMode argument
+  _ = try AES(key: key, blockMode: blockMode, padding: padding)
+  _ = try AES(key: key, blockMode: blockMode)
+  _ = try Blowfish(key: key, blockMode: blockMode, padding: padding)
+}
+
+func test_r1_simple_no_violation() throws {
+  let key = getRandomArray()
+  let padding = Padding.noPadding
+  let blockMode = CBC(iv: getRandomArray())
+  
+  // Violation SHOULD NOT be detected
+  _ = try AES(key: key, blockMode: blockMode, padding: padding)
+  _ = try AES(key: key, blockMode: blockMode)
+  _ = try Blowfish(key: key, blockMode: blockMode, padding: padding)
+}
+
+// *** RULE 2: NO NON-RANDOM IVS ***
+
+func test_r2_simple_violation() throws {
+  let iv = getConstantArray()
+  let ivString = iv.toHexString()
+  let key = getRandomArray()
+  let keyString = key.toHexString()
+  let randomArray = getRandomArray()
+  
+  // Violation SHOULD be detected only for iv argument
+  
+  // BlockModes
+  _ = CBC(iv: iv)
+  _ = CFB(iv: iv)
+  _ = CFB(iv: iv, segmentSize: CFB.SegmentSize.cfb8)
+  _ = CCM(iv: iv, tagLength: 0, messageLength: 0, additionalAuthenticatedData: randomArray)
+  _ = CCM(iv: iv, tagLength: 0, messageLength: 0, authenticationTag: randomArray, additionalAuthenticatedData: randomArray)
+  _ = OFB(iv: iv)
+  _ = CTR(iv: iv)
+  _ = CTR(iv: iv, counter: 0)
+  _ = PCBC(iv: iv)
+  
+  // Cryptors
+  _ = try AES(key: keyString, iv: ivString)
+  _ = try ChaCha20(key: key, iv: iv)
+  _ = try ChaCha20(key: keyString, iv: ivString)
+  _ = try Blowfish(key: keyString, iv: ivString)
+  _ = try Blowfish(key: keyString, iv: ivString, padding: Padding.noPadding)
+  _ = try Rabbit(key: key, iv: iv)
+  _ = try Rabbit(key: keyString, iv: ivString)
+}
+
+func test_r2_simple_no_violation() throws {
+  let iv = getRandomArray()
+  let ivString = iv.toHexString()
+  let key = getRandomArray()
+  let keyString = key.toHexString()
+  let randomArray = getRandomArray()
+  
+  // Violation SHOULD NOT be detected
+  
+  // BlockModes
+  _ = CBC(iv: iv)
+  _ = CFB(iv: iv)
+  _ = CFB(iv: iv, segmentSize: CFB.SegmentSize.cfb8)
+  _ = CCM(iv: iv, tagLength: 0, messageLength: 0, additionalAuthenticatedData: randomArray)
+  _ = CCM(iv: iv, tagLength: 0, messageLength: 0, authenticationTag: randomArray, additionalAuthenticatedData: randomArray)
+  _ = OFB(iv: iv)
+  _ = CTR(iv: iv)
+  _ = CTR(iv: iv, counter: 0)
+  _ = PCBC(iv: iv)
+  
+  // Cryptors, also test the different *.randomIV options
+  let aesIV = AES.randomIV(10)
+  _ = try AES(key: keyString, iv: aesIV.toHexString())
+  let chachaIV = ChaCha20.randomIV(10)
+  _ = try ChaCha20(key: key, iv: chachaIV)
+  _ = try ChaCha20(key: keyString, iv: chachaIV.toHexString())
+  _ = try Blowfish(key: keyString, iv: ivString)
+  _ = try Blowfish(key: keyString, iv: ivString, padding: Padding.noPadding)
+  _ = try Rabbit(key: key, iv: iv)
+  _ = try Rabbit(key: keyString, iv: ivString)
+}
+
+func test_r2_non_random_but_not_constant_violation() {
+  let iv = getUnknownArray()
+  // Violation SHOULD be detected only for iv argument
+  _ = CBC(iv: iv)
+}
+
+// *** RULE 3: DO NOT USE CONSTANT ENCRYPTION KEYS ***
+
+func test_r3_simple_violation() throws {
+  let key = getConstantArray()
+  let randomArray = getRandomArray()
+  let keyString = getConstantString()
+  let blockMode = CBC(iv: randomArray)
+  let padding = Padding.noPadding
+  let variant = HMAC.Variant.sha2(.sha256)
+  let iv = AES.randomIV(10)
+  let ivString = iv.toHexString()
+  
+  // Violation SHOULD be detected only for key argument
+  
+  // AES
+  _ = try AES(key: key, blockMode: blockMode, padding: padding)
+  _ = try AES(key: key, blockMode: blockMode)
+  _ = try AES(key: keyString, iv: ivString)
+  _ = try AES(key: keyString, iv: ivString, padding: padding)
+
+  // HMAC
+  _ = HMAC(key: key)
+  _ = HMAC(key: key, variant: variant)
+  _ = try HMAC(key: keyString)
+  _ = try HMAC(key: keyString, variant: variant)
+  
+  // ChaCha20
+  _ = try ChaCha20(key: key, iv: iv)
+  _ = try ChaCha20(key: keyString, iv: ivString)
+  
+  // CBCMAC
+  _ = try CBCMAC(key: key)
+  
+  // CMAC
+  _ = try CMAC(key: key)
+
+  // Poly1305
+  _ = Poly1305(key: key)
+  
+  // Blowfish
+  _ = try Blowfish(key: keyString, iv: ivString)
+  _ = try Blowfish(key: keyString, iv: ivString, padding: padding)
+  _ = try Blowfish(key: key, blockMode: blockMode, padding: padding)
+  _ = try Blowfish(key: key, padding: padding)
+  
+  // Rabbit
+  _ = try Rabbit(key: key)
+  _ = try Rabbit(key: keyString)
+  _ = try Rabbit(key: key, iv: iv)
+  _ = try Rabbit(key: keyString, iv: ivString)
+}
+
+func test_r3_simple_no_violation() throws {
+  let key = getRandomArray()
+  let randomArray = getRandomArray()
+  let keyString = AES.randomIV(10).toHexString()
+  let blockMode = CBC(iv: randomArray)
+  let padding = Padding.noPadding
+  let variant = HMAC.Variant.sha2(.sha256)
+  let iv = AES.randomIV(10)
+  let ivString = iv.toHexString()
+  
+  // Violation SHOULD NOT be detected
+  
+  // AES
+  _ = try AES(key: key, blockMode: blockMode, padding: padding)
+  _ = try AES(key: key, blockMode: blockMode)
+  _ = try AES(key: keyString, iv: ivString)
+  _ = try AES(key: keyString, iv: ivString, padding: padding)
+  
+  // HMAC
+  _ = HMAC(key: key, variant: variant)
+  _ = try HMAC(key: keyString)
+  _ = try HMAC(key: keyString, variant: variant)
+  _ = HMAC(key: key)
+  
+  // ChaCha20
+  _ = try ChaCha20(key: key, iv: iv)
+  _ = try ChaCha20(key: keyString, iv: ivString)
+  
+  // CBCMAC
+  _ = try CBCMAC(key: key)
+  
+  // CMAC
+  _ = try CMAC(key: key)
+
+  // Poly1305
+  _ = Poly1305(key: key)
+  
+  // Blowfish
+  _ = try Blowfish(key: keyString, iv: ivString)
+  _ = try Blowfish(key: keyString, iv: ivString, padding: padding)
+  _ = try Blowfish(key: key, blockMode: blockMode, padding: padding)
+  _ = try Blowfish(key: key, padding: padding)
+  
+  // Rabbit
+  _ = try Rabbit(key: key)
+  _ = try Rabbit(key: keyString)
+  _ = try Rabbit(key: key, iv: iv)
+  _ = try Rabbit(key: keyString, iv: ivString)
+}
+
+// *** RULE 4: CONSTANT SALT ***
+
+func test_r4_simple_violation() throws {
+  let salt = getConstantArray()
+  let iterations = getGoodIterationCount()
+  let randomArray = getRandomArray()
+
+  // Violation SHOULD be detected only for password argument
+  
+  _ = try HKDF(password: randomArray, salt: salt, info: randomArray, keyLength: 0, variant: HMAC.Variant.sha2(.sha256))
+  _ = try PKCS5.PBKDF1(password: randomArray, salt: salt, iterations: iterations, keyLength: 0)
+  _ = try PKCS5.PBKDF2(password: randomArray, salt: salt, iterations: iterations, keyLength: 0)
+  _ = try Scrypt(password: randomArray, salt: salt, dkLen: 64, N: 16384, r: 8, p: 1)
+}
+
+func test_r4_from_string_violation() throws {
+  let constantString = getConstantString()
+  let salt = constantString.bytes
+  let randomArray = getRandomArray()
+
+  // Violation SHOULD be detected only for salt argument
+  _ = try HKDF(password: randomArray, salt: salt, info: randomArray, keyLength: 0, variant: HMAC.Variant.sha2(.sha256))
+}
+
+func test_r4_from_string_md5_violation() throws {
+  let constantString = getConstantString()
+  let salt = constantString.bytes.md5()
+  let randomArray = getRandomArray()
+  
+  // Violation SHOULD be detected only for salt argument
+  _ = try HKDF(password: randomArray, salt: salt, info: randomArray, keyLength: 0, variant: HMAC.Variant.sha2(.sha256))
+}
+
+func test_r4_multiple_values_violation() throws {
+  let salt = unknownCondition() ? getConstantString().bytes : getRandomArray()
+  let randomArray = getRandomArray()
+  
+  // Violation SHOULD be detected only for salt argument
+  _ = try HKDF(password: randomArray, salt: salt, info: randomArray, keyLength: 0, variant: HMAC.Variant.sha2(.sha256))
+}
+
+func test_r4_simple_no_violation() throws {
+  let salt = getRandomArray()
+  let iterations = getGoodIterationCount()
+  let randomArray = getRandomArray()
+  
+  // Violation SHOULD NOT be detected
+  
+  _ = try HKDF(password: randomArray, salt: salt, info: randomArray, keyLength: 0, variant: HMAC.Variant.sha2(.sha256))
+  _ = try PKCS5.PBKDF1(password: randomArray, salt: salt, iterations: iterations, keyLength: 0)
+  _ = try PKCS5.PBKDF2(password: randomArray, salt: salt, iterations: iterations, keyLength: 0)
+  _ = try Scrypt(password: randomArray, salt: salt, dkLen: 64, N: 16384, r: 8, p: 1)
+}
+
+
+// *** RULE 5: DO NOT USE < 1000 ITERATIONS FOR PBE ***
+
+func test_r5_simple_violation() throws {
+  let iterations = getLowIterationCount()
+  let randomArray = getRandomArray()
+  
+  // Violation SHOULD be detected only for iterations argument
+  _ = try PKCS5.PBKDF1(password: randomArray, salt: randomArray, iterations: iterations, keyLength: 0)
+  _ = try PKCS5.PBKDF2(password: randomArray, salt: randomArray, iterations: iterations, keyLength: 0)
+}
+
+func test_r5_multiple_values_violation() throws {
+  let iterations = unknownCondition() ? getLowIterationCount() : getGoodIterationCount()
+  let randomArray = getRandomArray()
+    
+  // Violation SHOULD be detected only for iterations argument
+  _ = try PKCS5.PBKDF1(password: randomArray, salt: randomArray, iterations: iterations, keyLength: 0)
+}
+
+func test_r5_simple_no_violation() throws {
+  let iterations = getGoodIterationCount()
+  let randomArray = getRandomArray()
+  
+  // Violation SHOULD NOT be detected
+  _ = try PKCS5.PBKDF1(password: randomArray, salt: randomArray, iterations: iterations, keyLength: 0)
+  _ = try PKCS5.PBKDF2(password: randomArray, salt: randomArray, iterations: iterations, keyLength: 0)
+}
+
+// *** RULE 7: CONSTANT PASSWORD ***
+
+func test_r7_simple_violation() throws {
+  let password = getConstantArray()
+  let iterations = getGoodIterationCount()
+  let randomArray = getRandomArray()
+
+  // Violation SHOULD be detected only for password argument
+  
+  _ = try HKDF(password: password, salt: randomArray, info: randomArray, keyLength: 0, variant: HMAC.Variant.sha2(.sha256))
+  _ = try PKCS5.PBKDF1(password: password, salt: randomArray, iterations: iterations, keyLength: 0)
+  _ = try PKCS5.PBKDF2(password: password, salt: randomArray, iterations: iterations, keyLength: 0)
+  _ = try Scrypt(password: password, salt: randomArray, dkLen: 64, N: 16384, r: 8, p: 1)
+}
+
+func test_r7_from_string_violation() throws {
+  let constantString = getConstantString()
+  let password = constantString.bytes
+  let randomArray = getRandomArray()
+
+  // Violation SHOULD be detected only for password argument
+  _ = try HKDF(password: password, salt: randomArray, info: randomArray, keyLength: 0, variant: HMAC.Variant.sha2(.sha256))
+}
+
+func test_r7_from_string_md5_violation() throws {
+  let constantString = getConstantString()
+  let password = constantString.bytes.md5()
+  let randomArray = getRandomArray()
+
+  // Violation SHOULD be detected only for password argument
+  _ = try HKDF(password: password, salt: randomArray, info: randomArray, keyLength: 0, variant: HMAC.Variant.sha2(.sha256))
+}
+
+func test_r7_multiple_values_violation() throws {
+  let constantString = getConstantString()
+  let password = unknownCondition() ? constantString.bytes : getRandomArray()
+  let randomArray = getRandomArray()
+
+  // Violation SHOULD be detected only for password argument
+  _ = try HKDF(password: password, salt: randomArray, info: randomArray, keyLength: 0, variant: HMAC.Variant.sha2(.sha256))
+}
+
+func test_r7_simple_no_violation() throws {
+  let password = getRandomArray()
+  let iterations = getGoodIterationCount()
+  let randomArray = getRandomArray()
+  
+  // Violation SHOULD NOT be detected
+  _ = try HKDF(password: password, salt: randomArray, info: randomArray, keyLength: 0, variant: HMAC.Variant.sha2(.sha256))
+  _ = try PKCS5.PBKDF1(password: password, salt: randomArray, iterations: iterations, keyLength: 0)
+  _ = try PKCS5.PBKDF2(password: password, salt: randomArray, iterations: iterations, keyLength: 0)
+  _ = try Scrypt(password: password, salt: randomArray, dkLen: 64, N: 16384, r: 8, p: 1)
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,80 @@
-# swan-crypto
-This repo contains work for Daniil's thesis on using SWAN for crypto API misuse
+
+
+# SWAN Crypto
+
+This repository contains work for using [SWAN](https://github.com/themaplelab/swan) to find cryptographic API misuses. This work is experimental.
+
+### Summary
+
+I have extended SWAN to feature hand-crafted analyses for detecting misuses in the popular open-source [SwiftCrypto](https://github.com/krzyzanowskim/CryptoSwift) API. You can find the analysis code [here](https://github.com/themaplelab/swan/blob/crypto/jvm/ca.ualberta.maple.swan.spds/src/scala/ca/ualberta/maple/swan/spds/analysis/crypto/CryptoAnalysis.scala). My analyses follow the classic crypto rules/guidelines as laid out in the following works:
+
+- Modelling Analysis and Auto-detection of Cryptographic Misuse in Android Applications [[link](https://ieeexplore.ieee.org/document/6945307)]
+- A Comparative Study of Misapplied Crypto in Android and iOS Applications [[link](https://www.semanticscholar.org/paper/A-Comparative-Study-of-Misapplied-Crypto-in-Android-Feichtner/d3c48ad2e7e67521f5847f596ab8b3ca37f6b5a4)]
+- An empirical study of cryptographic misuse in android applications [[link](https://dl.acm.org/doi/10.1145/2508859.2516693)]
+
+My analysis currently supports the following rules:
+
+1. Do Not Use ECB Mode for Encryption
+
+   ```swift
+   let blockMode = ECB()
+   _ = try AES(key: key, blockMode: blockMode, padding: padding)
+   ```
+
+2. No Non-random IVs for Encryption
+
+   ```swift
+   let iv = "constant string"
+   _ = try AES(key: key, iv: iv)
+   ```
+
+3.  Do Not Use Constant Encryption Keys
+
+   ```swift
+   let key = "constant key".bytes
+   _ = try AES(key: key, blockMode: blockMode, padding: padding)
+   ```
+
+4. Do Not Use Constant Salts for PBE
+
+   ```swift
+   let salt = "constant salt".bytes
+   _ = try HKDF(password: pwd, salt: salt, info: i, keyLength: 128, variant: .sha2(.sha256))
+   ```
+
+5. Do Not Use < 1000 Iterations for PBE
+
+   ```swift
+   let iterations = 500
+   _ = try PKCS5.PBKDF1(password: pwd, salt: salt, iterations: iterations, keyLength: 128)
+   ```
+
+6. Do Not Use Constant Password for PBE
+
+   ```swift
+   let password = "constant password".bytes
+   _ = try HKDF(password: password, salt: salt, info: i, keyLength: 128, variant: .sha2(.sha256))
+   ```
+
+## Instructions
+
+Clone SWAN using the `crypto` branch.
+
+```shell
+git clone https://github.com/themaplelab/swan.git -b crypto
+```
+
+Follow the instructions located in the README of SWAN to build SWAN. The `lib/` directory will contain the executables you need to run SWAN. I recommend putting the `lib/` directory onto your `$PATH`.
+
+### Running tests
+
+`CryptoSwiftTests/` contains an Xcode project with code that exhibits API misuses for use with the crypto analysis. You can run the analysis on the project using the following series of commands. Currently, analysis results are only written to the terminal.
+
+```
+cd CryptoSwiftTests/
+swan-xcodebuild -- -project CryptoSwiftTests.xcodeproj -scheme CryptoSwiftTests
+cp ../sil/CryptoSwift.CryptoSwift.sil swan-dir/
+java -jar driver.jar --crypto swan-dir/
+```
+
+Note: Due to an issue with parsing, you need to copy  the `CryptoSwift.CryptoSwift.sil` file located in `sil/` into the ` swan-dir/`.
